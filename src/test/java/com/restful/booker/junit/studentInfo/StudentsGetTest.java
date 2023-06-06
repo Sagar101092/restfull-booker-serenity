21d76ae643059ad556ef90a7d4763c763c3746b8
Sagar101092 <Samgowswami90@gmail.com>
2023-06-06 23:20:10 +0100

    Week18 Homework

---
+++ src/test/java/com/restful/booker/junit/studentInfo/StudentsGetTest.java
@@ -0,0 +1,12 @@
+package com.restful.booker.junit.studentInfo;
+
+
+import com.restful.booker.testbase.TestBase;
+
+/**
+ * Created by Jay
+ */
+public class StudentsGetTest extends TestBase {
+
+
+}
---
+++ src/test/java/com/restful/booker/junit/studentidinfo/StudentsGetTest1.java
@@ -0,0 +1,11 @@
+package com.restful.booker.junit.studentidinfo;
+
+
+import com.restful.booker.testbase.TestBase;
+
+/**
+ * Created by Jay
+ */
+public class StudentsGetTest1 extends TestBase {
+
+}
